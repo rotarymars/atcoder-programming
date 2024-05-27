bef168f5de09ba0786a0c8e6e90b234119f5c6d1
rotarymars <s.goto2050@gmail.com>
2024-05-27 23:32:28 +0900

    commit

---
+++ download.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+import pathlib
+import os
+import re
+LOGIN_URL = "https://atcoder.jp/login"
+def getsession():
+    session = requests.session()
+    result = session.get(LOGIN_URL)
+    soup = BeautifulSoup(result.text,"lxml")
+    csrf_token = soup.find(attrs={"name": "csrf_token"}).get("value")
+    username = input("Input your username: ")
+    password = input("Input your password: ")
+    login_info = {
+    "csrf_token": csrf_token,
+    "username": username,
+    "password": password,
+    "continue": "https://atcoder.jp"
+    }
+    result = session.post(LOGIN_URL,data=login_info)
+    if result.status_code==200:
+        return session
+    else:
+        return -1
+def returnurl():
+    nowdirectory=pathlib.Path(os.getcwd()).name
+    for i in range(len(nowdirectory)):
+        if nowdirectory[len(nowdirectory) - 1 - i] == "_":
+            return f"https://atcoder.jp/contests/{nowdirectory[0:(len(nowdirectory)-1-i)]}/tasks/{nowdirectory}"
+def savetotest(ar):
+    pathtotest = "./test"
+    for i in range(len(ar)//2):
+        with open(f"{pathtotest}/sample-{i}.in",mode="w",newline="\n") as f:
+            f.write(ar[2 * i])
+        with open(f"{pathtotest}/sample-{i}.out",mode="w",newline="\n") as f:
+            f.write(ar[2 * i + 1])
+def main():
+    mysession=getsession()
+    if mysession == -1:
+        print("Failed to get session. Aborting...")
+        return 1
+    problem_url = returnurl()
+    print(problem_url)
+    result = mysession.get(problem_url)
+    #print(result.text)
+    soup = BeautifulSoup(result.text,"lxml")
+    #print("----------------------")
+    rawsample = list(soup.find_all("pre"))
+    sample = []
+    for i in rawsample:
+        sample.append(i.string)
+    sample = sample[1 : (len(sample)//2)]
+    print(sample)
+    savetotest(sample)
+
+if __name__=="__main__":
+    main()
